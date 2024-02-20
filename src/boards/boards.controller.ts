f581af09666d6db40fda931e1209cf2e1acd4087
Ksiyeong <philip2767@gmail.com>
2024-02-20 22:58:32 +0900

    Feat: boards.controller

---
+++ src/boards/boards.controller.ts
@@ -0,0 +1,20 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { BoardsService } from './boards.service';
+import { Board } from './board.model';
+import { CreateBoardDto } from './dto/create-board.dto';
+
+@Controller('boards')
+export class BoardsController {
+    constructor(private boardsService: BoardsService) { }
+
+    @Get()
+    getAllBoards(): Board[] {
+        return this.boardsService.getAllBoards();
+    }
+
+    @Post()
+    createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+        return this.boardsService.createBoard(createBoardDto);
+    }
+
+}
