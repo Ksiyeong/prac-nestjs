4dfb199816eeeb11796c8920fe4ae680f96c966c
Ksiyeong <philip2767@gmail.com>
2024-02-21 17:39:18 +0900

    Feat: Delete /boards/:id 게시글 삭제 기능 구현

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -24,6 +24,10 @@ export class BoardsService {
     }
 
     getBoardById(id: string): Board {
-        return this.boards.find((board) => board.id === id);
+        return this.boards.find(board => board.id === id);
+    }
+
+    deleteBoard(id: string): void {
+        this.boards = this.boards.filter(board => board.id !== id);
     }
 }
