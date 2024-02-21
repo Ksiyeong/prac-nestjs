777cfc443d1f34781e8e12f8e217dd0435da9a3f
Ksiyeong <philip2767@gmail.com>
2024-02-21 20:25:04 +0900

    Feat: PATCH /boards/:id/status status 유효성 검사 추가

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -2,16 +2,17 @@ import { Body, Controller, Delete, Get, Param, ParseUUIDPipe, Patch, Post, UsePi
 import { BoardsService } from './boards.service';
 import { Board, BoardStatus } from './board.model';
 import { CreateBoardDto } from './dto/create-board.dto';
+import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
 
 @Controller('boards')
 export class BoardsController {
     constructor(private boardsService: BoardsService) { }
-    
+
     @Get()
     getAllBoards(): Board[] {
         return this.boardsService.getAllBoards();
     }
-    
+
     @Post()
     @UsePipes(ValidationPipe)
     createBoard(@Body() createBoardDto: CreateBoardDto): Board {
@@ -31,7 +32,8 @@ export class BoardsController {
     @Patch('/:id/status')
     updateBoardStatus(
         @Param('id') id: string,
-        @Body('status') status: BoardStatus): Board {
+        @Body('status', BoardStatusValidationPipe) status: BoardStatus
+    ): Board {
         return this.boardsService.updateBoardStatus(id, status);
     }
 }
