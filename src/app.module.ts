a75526af68cf22e7e23457a23407583fd1494d9c
Ksiyeong <philip2767@gmail.com>
2024-02-21 23:03:54 +0900

    Feat: 환경변수 사용 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BoardsModule } from './boards/boards.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [BoardsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    BoardsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
