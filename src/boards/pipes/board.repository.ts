9e2c38dc99bb7548f887acd0942bfbe60ea34648
Ksiyeong <philip2767@gmail.com>
2024-02-22 13:26:08 +0900

    Feat: board.repository.ts

---
+++ src/boards/pipes/board.repository.ts
@@ -0,0 +1,9 @@
+import { DataSource, Repository } from "typeorm";
+import { Board } from "../board.entity";
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export class BoardRepository extends Repository<Board>{
+    constructor(private dataSource: DataSource) { super(Board, dataSource.createEntityManager()) }
+
+}
