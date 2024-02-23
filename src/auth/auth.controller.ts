ee1f488d33dd47da8b3335c0649cb1141479c6e0
Ksiyeong <philip2767@gmail.com>
2024-02-23 18:24:53 +0900

    Feat: POST /auth/signup 회원가입 기능 구현

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthCredentialsDto } from './dto/auth-credential.dto';
+import { User } from './user.entity';
 
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) { }
 
+    @Post('/signup')
+    @UsePipes(ValidationPipe)
+    signUp(@Body() authCredentialsDto: AuthCredentialsDto): Promise<User> {
+        return this.authService.signUp(authCredentialsDto);
+    }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { UserRepository } from './user.repository';
+import { AuthCredentialsDto } from './dto/auth-credential.dto';
+import { User } from './user.entity';
 
 @Injectable()
 export class AuthService {
     constructor(private readonly userRepository: UserRepository) { }
-    
+
+    async signUp(authCredentialsDto: AuthCredentialsDto): Promise<User> {
+        return this.userRepository.createUser(authCredentialsDto);
+    }
 }
