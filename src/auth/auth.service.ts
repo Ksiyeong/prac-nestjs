0033687a4f91b87f297f658bd4c248ff0921678b
Ksiyeong <philip2767@gmail.com>
2024-02-24 22:23:17 +0900

    Feat: /auth/signin 회원가입 기능 구현

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserRepository } from './user.repository';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
-import { User } from './user.entity';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
@@ -20,5 +19,16 @@ export class AuthService {
         const salt = await bcrypt.genSalt();
         authCredentialsDto.password = await bcrypt.hash(authCredentialsDto.password, salt);
         return this.userRepository.createUser(authCredentialsDto);
+    }
+
+    async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+        const { username, password } = authCredentialsDto;
+        const user = await this.userRepository.findOneBy({ username });
+
+        if (user && await bcrypt.compare(password, user.password)) {
+            return 'login success';
+        } else {
+            throw new UnauthorizedException('login failed');
+        }
     }
 }
