c3dbcf0962f75e1bffc4f16e9be5525e44aead22
Ksiyeong <philip2767@gmail.com>
2024-02-24 22:10:22 +0900

    Refactor: signUp return 값 변경 Promise<void> 비밀번호 암호화

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { UserRepository } from './user.repository';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
 import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -11,10 +12,13 @@ export class AuthService {
         return await this.userRepository.existsBy({ username });
     }
 
-    /*async*/ signUp(authCredentialsDto: AuthCredentialsDto): Promise<User> {
+    async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
         // if (await this.existsByUsername(authCredentialsDto.username)) {
         //     throw new ConflictException('username already exists');
         // }
+
+        const salt = await bcrypt.genSalt();
+        authCredentialsDto.password = await bcrypt.hash(authCredentialsDto.password, salt);
         return this.userRepository.createUser(authCredentialsDto);
     }
 }
