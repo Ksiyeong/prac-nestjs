7be11dfa6cc36e1a850010d168ccafb01aed2389
Ksiyeong <philip2767@gmail.com>
2024-02-26 17:56:19 +0900

    Feat: User - Board 1대N 연관관계매핑

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,4 +1,5 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Board } from "src/boards/board.entity";
+import { BaseEntity, Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity('users')
 export class User extends BaseEntity {
@@ -12,4 +13,6 @@ export class User extends BaseEntity {
     @Column()
     password: string;
 
+    @OneToMany(type => Board, board => board.user, { eager: true })
+    boards: Board[];
 }
