54e65662c085103b177904427b5523113c536812
Ksiyeong <philip2767@gmail.com>
2024-02-27 12:01:46 +0900

    Refactor: 로그 getAllboards, createBoard

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Logger, Param, ParseIntPipe, Patch, Post, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { BoardStatus } from './board-status.enum';
 import { CreateBoardDto } from './dto/create-board.dto';
@@ -11,10 +11,13 @@ import { User } from 'src/auth/user.entity';
 @Controller('boards')
 @UseGuards(AuthGuard())
 export class BoardsController {
+    private logger = new Logger(BoardsController.name);
     constructor(private boardsService: BoardsService) { }
 
     @Get()
     getAllBoards(@GetUser() user: User): Promise<Board[]> {
+
+        this.logger.verbose(`GET /boards ${this.getAllBoards.name} User: ${user.username}`);
         return this.boardsService.getAllBoards(user);
     }
 
@@ -23,6 +26,8 @@ export class BoardsController {
     createBoard(
         @Body() createBoardDto: CreateBoardDto,
         @GetUser() user: User): Promise<Board> {
+
+        this.logger.verbose(`POST /boards ${this.createBoard.name} User: ${user.username}, Payload: ${JSON.stringify(createBoardDto)}`);
         return this.boardsService.createBoard(createBoardDto, user);
     }
 
