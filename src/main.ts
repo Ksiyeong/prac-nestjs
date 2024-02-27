bb4b0ba39b96e8d7dda5d3fa5c17a0469cfa018f
Ksiyeong <philip2767@gmail.com>
2024-02-27 12:01:08 +0900

    Feat: 서버 부팅 로그

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const port = process.env.NODE_SERVER_PORT;
   const app = await NestFactory.create(AppModule);
   await app.listen(port);
+  Logger.log(`Application running on port ${port}`);
 }
 bootstrap();
